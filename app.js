fb7420c7320ec6b79641f4e22c16f0156491b6cd
miluferrini <miliferrini@gmail.com>
2023-04-20 20:31:49 -0300

    Creando repo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+
+app.use(express.static("public"));
+app.listen(3000,()=>{
+    console.log("Servidor escuchando en el puerto 3000 ✔");
+});
+
+app.get("/",(req,res) =>{
+    res.send("Estas en Home!");
+})
+
+app.get("/home", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+ 
