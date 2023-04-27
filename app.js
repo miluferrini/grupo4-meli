68085b2bf6afadac0e27f181a363eac67eaa9576
miluferrini <miliferrini@gmail.com>
2023-04-27 20:34:23 -0300

    Login Terminado

--- app.js
+++ app.js
@@ -18,3 +18,7 @@ app.get("/home", function(req,res){
 app.get("/register", function(req,res){
     res.sendFile(path.join(__dirname,"/views/register.html"));
 });
+
+app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+});
