218a30af0e5d5531a0efad07e6d581d3319503e4
GonzaloEsposito <gonza.2@hotmail.com>
2023-05-08 23:16:15 -0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 const app = express();
 const path = require ("path");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.listen(3000,()=>{
-    console.log("Servidor escuchando en el puerto 3000 ✔");
+app.listen(PORT,()=>{
+    console.log("Servidor corriendo en el puerto " + PORT);
 });
 
 app.get("/",(req,res) =>{
